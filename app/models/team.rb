77dfcb6d40cb2706ff662d202cd0663a75dc2c40
Andrew Kraut <akraut@gmail.com>
2009-10-12 00:35:10 -0500

    Added validation to Team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,7 @@
 class Team < ActiveRecord::Base
   has_and_belongs_to_many :players
+  
+  validates_presence_of :name, :sms_code
+  validates_length_of :name, :within => 5..32
+  validates_length_of :sms_code, :is => 2
 end
